14eaf766aeabdaf73b0844bb4ece4f9609e8e2a4
Maksim Korotkevich <m.korotkevich.dev@gmail.com>
2026-06-20 17:32:06 +0300

    feat(ui): add dynamic app link component

---
+++ app/components/shared/ui/AppLink.vue
@@ -0,0 +1,30 @@
+<template>
+    <component 
+    :is="element"
+    :to="to"
+    :href="href"
+    :target="isExternal ? '_blank': undefined"
+    :rel="isExternal? 'noopener noreferrer': undefined"
+    class='app-link'
+    >
+        <slot />
+    </component>
+</template>
+<script setup lang="ts">
+
+const props = defineProps<{
+    to?: string;
+    href?: string;
+}>();
+const isExternal = computed(() => !!props.href);
+const element = computed(() => {
+    if (props.to) return resolveComponent('NuxtLink');
+    return 'a';
+})
+</script>
+
+<style scoped lang="scss">
+.app-link{
+    cursor: pointer;
+}
+</style>
