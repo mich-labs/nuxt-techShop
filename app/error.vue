6eaa2e04161c800dbb4fc4da6165320c87d62d05
Maksim Korotkevich <m.korotkevich.dev@gmail.com>
2026-06-21 23:13:43 +0300

    feat(error): initial layout for error template (WIP)

---
+++ app/error.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app-wrapper">
+    <NuxtLayout>
+      <app-404 v-if="error.status === 404" />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app';
+
+const props = defineProps<{
+  error: NuxtError;
+}>();
+</script>
+
+<style scoped lang="scss">
+.app-wrapper {
+  border-radius: 20px 20px 0 0;
+
+  background-color: var(--bg-1);
+  -webkit-box-shadow: var(--app-shadow);
+  -moz-box-shadow: var(--app-shadow);
+  box-shadow: var(--app-shadow);
+}
+</style>
