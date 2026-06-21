c4d7ec3787677bfdf3e422aae4127e605dc415d2
Maksim Korotkevich <m.korotkevich.dev@gmail.com>
2026-06-21 20:56:06 +0300

    feat(ui): add banner component

---
+++ app/components/shared/ui/AppBanner.vue
@@ -0,0 +1,46 @@
+<template>
+  <app-link
+    :to="to"
+    :href="href"
+    class="banner"
+  >
+    <slot name="content" />
+    <NuxtImg
+      v-if="img"
+      :src="img"
+      format="webp"
+      :class="['banner__img', $slots.content ? 'banner__img--abs' : undefined]"
+      :alt="img ? alt : undefined"
+    />
+  </app-link>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  to?: string;
+  href?: string;
+  img: string;
+  alt?: string;
+}>();
+</script>
+
+<style scoped lang="scss">
+.banner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+
+  &__img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+
+    &--abs {
+      position: absolute;
+      left: 0;
+      top: 0;
+      z-index: -1;
+    }
+  }
+}
+</style>
