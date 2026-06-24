1e404775ae44436f47cb2787705d7e5e3022a2dd
m-ich-dev <m.korotkevich.dev@gmail.com>
2026-06-24 15:23:27 +0300

    refactor(ui): add wrapper for content in text-block component

--- app/components/shared/ui/AppTextBlock.vue
+++ app/components/shared/ui/AppTextBlock.vue
@@ -1,29 +1,37 @@
 <template>
-    <div class="text-block">
-        <h2 v-if="$slots.title" class="text-block__title">
-            <slot name="title" mdc-unwrap="p" />
-        </h2>
-
-        <slot name="text" />
+  <div class="text-block">
+    <h2
+      v-if="$slots.title"
+      class="text-block__title"
+    >
+      <slot
+        name="title"
+        mdc-unwrap="p"
+      />
+    </h2>
+    <div class="text-block__content">
+      <slot name="text" />
     </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
 .text-block {
+  &__title {
+    font-size: 2rem;
+    font-weight: 500;
+    letter-spacing: 0.05em;
+    margin-bottom: 20px;
+  }
+  &__content {
     font-size: 1.6rem;
     text-align: justify;
     line-height: 20px;
-    letter-spacing: -.02em;
+    letter-spacing: -0.02em;
+  }
 
-    &__title {
-        font-size: 2rem;
-        font-weight: 500;
-        letter-spacing: .05em;
-        margin-bottom: 20px;
-    }
-
-    :deep(p:not(:last-child)) {
-        margin-bottom: 10px;
-    }
+  :deep(p:not(:last-child)) {
+    margin-bottom: 10px;
+  }
 }
 </style>
