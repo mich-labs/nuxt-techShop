d23dd785d1d2348711a0788d6872108211e682b1
m-ich-dev <m.korotkevich.dev@gmail.com>
2026-06-24 15:57:07 +0300

    feat(widgets): introduce tupl section (WIP)

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
+  <app-stack direction="column" :gap="100">
     <the-hero-carousel />
+    <the-tupl-section />
+  </app-stack>
 </template>
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
