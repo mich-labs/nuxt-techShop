a9345b3b96f473f5f48e35bcb75a1cc11f4fc126
Maksim Korotkevich <m.korotkevich.dev@gmail.com>
2026-07-11 14:26:37 +0200

    refactor(page): introduce home page composable and implement data fetching

---
+++ app/composables/useHomePage.ts
@@ -0,0 +1,36 @@
+import { mapCarouselSection } from "~/components/widgets/hero-carousel";
+import { mapPromoSection } from "~/components/widgets/promo-section";
+
+
+export default async function useHomePage() {
+    const { public: { cmsUrl } } = useRuntimeConfig();
+    const { data } = await useAsyncData('homePage', async () => {
+        const { $cms } = useNuxtApp();
+        return await $cms.glavnaya.find({
+            populate: {
+                promoSection1: {
+                    populate: '*',
+                },
+                promoSection2: {
+                    populate: '*',
+                },
+                slider: {
+                    populate: {
+                        banners: {
+                            populate: '*',
+                        },
+                        button: true,
+                    },
+                },
+                seo: true,
+            },
+        });
+
+    })
+    return {
+        carouselData: data.value?.slider ? mapCarouselSection(data.value.slider, cmsUrl) : null,
+        promoSection1: data.value?.promoSection1 ? mapPromoSection(data.value.promoSection1, cmsUrl) : null,
+        promoSection2: data.value?.promoSection2 ? mapPromoSection(data.value.promoSection2, cmsUrl) : null,
+        seo: { title: data.value?.seo?.title || '', description: data.value?.seo?.description || '' },
+    }
+}
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,54 +3,26 @@
     direction="column"
     :gap="100"
   >
-    <the-hero-carousel />
-    <the-banner-section :reverse="true" />
+    <the-hero-carousel
+      v-if="data.carouselData"
+      :data="data.carouselData"
+    />
+    <the-promo-section
+      v-if="data.promoSection1"
+      :data="data.promoSection1"
+      :reverse="true"
+    />
+    <the-promo-section
+      v-if="data.promoSection2"
+      :data="data.promoSection2"
+    />
   </app-stack>
 </template>
 <script setup lang="ts">
-import type {
-  Glavnaya,
-  GlavnayaPopulateParam,
-  SharedBanner,
-  SharedBannerGetPayload,
-  SharedBannerPopulateParam,
-  SharedTekstovyjBlokDz,
-  WidgetsBannerSekcziya,
-  WidgetsBannerSekcziyaPopulateParam,
-} from '~~/cms';
+const data = await useHomePage();
 
-const { $cms } = useNuxtApp();
-
-const { data } = useAsyncData('home', async () => {
-  return await $cms.glavnaya.find({
-    populate: {
-      bannerSection1: {
-        populate: '*',
-      },
-      bannerSection2: {
-        populate: '*',
-      },
-      slider: {
-        populate: {
-          banner: {
-            populate: '*',
-          },
-          button: true,
-        },
-      },
-      seo: true,
-    },
-  });
-});
 useSeoMeta({
-  title: data.value?.seo?.title,
-  description: data.value?.seo?.description,
+  title: data.seo.title,
+  description: data.seo.description,
 });
-const test1231 = data.value?.bannerSection1;
-type test = typeof data.value;
-interface Itest {
-  test1: WidgetsBannerSekcziya & WidgetsBannerSekcziyaPopulateParam;
-  test2: Glavnaya & GlavnayaPopulateParam;
-}
-console.log('data', data.value);
 </script>
