c780c569b1f8357abf69e1fd8cd3b39bba58e597
Maksim Korotkevich <m.korotkevich.dev@gmail.com>
2026-07-12 14:18:12 +0200

    refactor(ui): optimize header components for mobile layout viewports

--- app/components/shared/ui/AppNav.vue
+++ app/components/shared/ui/AppNav.vue
@@ -1,127 +1,154 @@
 <template>
-    <nav :class="['nav', sizeClass, variantClass]">
-        <ul class="nav__list">
-            <li v-for="route in routes" :key="route.name" class="nav__item">
-                <app-link class="nav__link" :href="route.to">
-                    <app-svg v-if="icons" :icon="route.name" class="nav__icon" />
-                    {{ capitalize ? toCapitalize(route.title) : route.title }}
-                </app-link>
-            </li>
-        </ul>
-    </nav>
+  <nav :class="['nav', sizeClass, variantClass]">
+    <ul class="nav__list">
+      <li
+        v-for="route in routes"
+        :key="route.name"
+        class="nav__item"
+      >
+        <app-link
+          class="nav__link"
+          :href="route.to"
+        >
+          <app-svg
+            v-if="icons"
+            :icon="route.name"
+            class="nav__icon"
+          />
+          <span
+            v-if="captions"
+            class="nav__caption"
+          >
+            {{ capitalize ? toCapitalize(route.title) : route.title }}
+          </span>
+        </app-link>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script setup lang="ts">
 import toCapitalize from '~/utils/capitalize';
 import type { TRoutes } from '~~/types/routeLinks.type';
-const { capitalize = false, size, variant } = defineProps<{
-    routes: TRoutes,
-    capitalize?: boolean,
-    icons: boolean,
-    size?: 'small',
-    variant?: 'page'
-}>()
+const {
+  capitalize = false,
+  size,
+  variant,
+  captions = true,
+} = defineProps<{
+  routes: TRoutes;
+  capitalize?: boolean;
+  icons: boolean;
+  size?: 'small';
+  variant?: 'page';
+  captions?: boolean;
+}>();
 const sizeClass = computed(() => {
-    return size ? 'nav--' + size : ''
-
-})
+  return size ? 'nav--' + size : '';
+});
 const variantClass = computed(() => {
-    return variant ? 'nav--variant-' + variant : ''
-})
+  return variant ? 'nav--variant-' + variant : '';
+});
 </script>
 
 <style scoped lang="scss">
 .nav {
-    &__list {
-        display: flex;
-        gap: 20px;
+  &__list {
+    display: flex;
+    gap: 20px;
+  }
+
+  &__link {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    color: var(--text-1);
+    font-size: 1.6rem;
+
+    cursor: pointer;
+    transition: color linear var(--t-2);
+
+    &:hover {
+      color: var(--accent);
+
+      .nav__icon {
+        fill: var(--accent);
+      }
+    }
+  }
+
+  &__icon {
+    height: 25px;
+    width: auto;
+  }
+
+  &--small {
+    .nav__list {
+      gap: 10px;
     }
 
-    &__link {
-        display: flex;
-        align-items: center;
-        gap: 5px;
-        color: var(--text-1);
-        font-size: 1.6rem;
-
-        cursor: pointer;
-        transition: color linear var(--t-2);
-
-        &:hover {
-            color: var(--accent);
-
-            .nav__icon {
-                fill: var(--accent);
-            }
-        }
+    .nav__link {
+      font-size: 1.4rem;
     }
+  }
 
-    &__icon {
-        height: 25px;
-        width: auto;
-    }
+  .router-link-exact-active {
+    color: var(--accent);
+  }
 
-    &--small {
-        .nav__list {
-            gap: 10px;
-        }
-
-        .nav__link {
-            font-size: 1.4rem;
-        }
-    }
+  &--variant-page {
+    width: 100%;
+    background-color: var(--bg-2);
+    border-radius: var(--b-r);
+    padding: 10px;
 
     .router-link-exact-active {
-        color: var(--accent);
+      background-color: var(--ui-3);
+      color: var(--accent);
     }
 
-    &--variant-page {
-        width: 100%;
-        background-color: var(--bg-2);
-        border-radius: var(--b-r);
-        padding: 10px;
+    .nav {
+      &__list {
+        flex-direction: column;
+        gap: 0;
+      }
 
-
-        .router-link-exact-active {
-            background-color: var(--ui-3);
-            color: var(--accent);
+      &__item {
+        &:last-child {
+          .nav__link {
+            border-bottom: none;
+            border-bottom-left-radius: calc(var(--b-r) / 2);
+            border-bottom-right-radius: calc(var(--b-r) / 2);
+          }
         }
 
-        .nav {
-
-            &__list {
-                flex-direction: column;
-                gap: 0;
-            }
-
-            &__item {
-                &:last-child {
-                    .nav__link {
-                        border-bottom: none;
-                        border-bottom-left-radius: calc(var(--b-r)/2);
-                        border-bottom-right-radius: calc(var(--b-r)/2);
-                    }
-                }
-
-                &:first-child {
-                    .nav__link {
-                        border-top-left-radius: calc(var(--b-r)/2);
-                        border-top-right-radius: calc(var(--b-r)/2);
-                    }
-                }
-            }
-
-            &__link {
-                padding: 15px 10px;
-                border-bottom: 1px solid var(--ui-1);
-                transition: background-color linear var(--t-2),
-                    color linear var(--t-2);
-
-                &:hover {
-                    background-color: var(--ui-3);
-                }
-            }
+        &:first-child {
+          .nav__link {
+            border-top-left-radius: calc(var(--b-r) / 2);
+            border-top-right-radius: calc(var(--b-r) / 2);
+          }
         }
+      }
+
+      &__link {
+        padding: 15px 10px;
+        border-bottom: 1px solid var(--ui-1);
+        transition:
+          background-color linear var(--t-2),
+          color linear var(--t-2);
+
+        &:hover {
+          background-color: var(--ui-3);
+        }
+      }
     }
+  }
+}
+@include device('sm') {
+  .nav {
+    &__caption {
+      display: none;
+    }
+  }
 }
 </style>
